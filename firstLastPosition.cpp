815e1a70f4199dc256fd166bb9b89bd215851c79
Pallavi Singh <48282655+pallavi223@users.noreply.github.com>
2022-07-08 20:55:51 +0530

    BinarySearchProblem

---
+++ firstLastPosition.cpp
@@ -0,0 +1,65 @@
+Find First and Last occurence of the array element 
+input--> arr[]- {1,2,3,3,5}
+         key = 3
+           
+ output - first occ: 2
+          last occ: 3
+            
+ 
+#include <iostream>
+#include <vector>
+using namespace std;
+int firstOcc(int arr[], int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            e = mid - 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int lastOcc(int arr[], int n, int key) {
+
+    int s = 0, e = n-1;
+    int mid = s + (e-s)/2;
+    int ans = -1;
+    while(s<=e) {
+
+        if(arr[mid] == key){
+            ans = mid;
+            s = mid + 1;
+        }
+        else if(key > arr[mid]) {//Right me jao
+            s = mid + 1;
+        }
+        else if(key < arr[mid]) {//left me jao
+            e = mid - 1;
+        }
+
+        mid = s + (e-s)/2;
+    }
+    return ans;
+}
+
+int main()
+{
+   int arr[5] = {1,2,3,3,5};
+    cout<<firstOcc(arr, 5, 3)<<endl;
+    cout<<lastOcc(arr, 5, 3);
+    
+    return 0;
+}
